bfbe41ac2bcd0ab81aae26165a4cea21cc778a2f
Laura Pinto <laurampinto90@gmail.com>
2024-06-20 20:40:16 +0100

    WDPT-FEB2024-Laura

--- src/index.js
+++ src/index.js
@@ -2,37 +2,67 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 
-
-
+document.addEventListener("click", () => {
+  const startButton = document.getElementById("start-btn");
+  startButton.addEventListener("click", startCountdown);
+});
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
+  const timeDisplay = document.getElementById("time");
+  const startButton = document.getElementById("start-btn");
+
+  startButton.disable = true;
+
+  timer = setInterval(() => {
+    remainingTime--;
+    timeDisplay.textContent = remainingTime;
+
+    if (remainingTime <= 0) {
+      clearInterval(timer);
+      startButton.disable = false;
+      remainingTime = DURATION;
+      showToast("Lift off! 🚀");
+    }
+  }, 1000);
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
+  const toast = document.getElementById("toast");
+  const toastMessage = document.getElementById("toast-message");
 
+  toastMessage.textContent = message;
+  toast.classList.add("show");
 
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  toastTimeout = setTimeout(() => {
+    hideToast();
+  }, 3000);
 }
+
+function hideToast() {
+  const toast = document.getElementById("toast");
+  toast.classList.remove("show");
+}
+
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+
+// Your code goes here ...
+document.addEventListener("click", () => {
+  const closeButton = document.getElementById("close-toast");
+
+  closeButton.addEventListener("click", () => {
+    hideToast();
+    clearTimeout(toastTimeout);
+  });
+});
